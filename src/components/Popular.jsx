0f6c28a54599c3f37d983bd217dbf5036f8f5b31
ConstHrach <hrachya62799@gmail.com>
2023-02-05 07:16:25 -0800

    added packages for styling as well as simple styling to understand how basic layout looks

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
-
 const [popular, setPopular] = useState([]);
 
 //running get popular as soon as compenent gets prepared
@@ -14,20 +14,36 @@ const getPopular = async () => { //fethces recipes from api including api key to
   const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
   const data = await api.json();
   console.log(data);
-  setPopular(data.recipes)
+  setPopular(data.recipes);
+  console.log(data.recipes)
 }
 // creates a div for each unique recipe based on id, creates a paragraph based on recipes title (without key prop it would bring errors)
   return <div>
   
     {popular.map((recipe) => {
       return(
-        <div key={recipe.id}>
-          <p>{recipe.title}</p>
-        </div>
+        <Wrapper>
+          <h3>Popular Picks!</h3>
+          {popular.map((recipe) => {
+            return(
+              <Card>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              </Card>
+            )
+          })}
+        </Wrapper>
       );
     })}
   </div>;
-  
 }
 
+const Wrapper = styled.div`
+margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+min-height: 25rem;
+border-radius: 2rem;
+`
 export default Popular;
