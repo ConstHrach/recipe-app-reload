26fda3c864c1bf587599aa6a35e8a235cc2d9d35
ConstHrach <hrachya62799@gmail.com>
2023-02-07 19:42:00 -0800

    bug resolved, K in apiKey had to be uppercase. search bar and recent searches added

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,55 +1,53 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Searched() {
-    const [searchRecipes, setSearchRecipes] = useState([]);
-    let params = useParams();
-    const getSearched = async (name) => {
-        const data = await fetch(
-             `https://api.spoonacular.com/recipes/complexSearch?apikey=${process.env.REACT_APP_API_KEY}&query=${name}
-             `);
-             const recipes = await data.json();
-             setSearchRecipes(recipes.results);
-    };
+   
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+  let params = useParams();
 
-    useEffect(()=>{
-        getSearched(params.search);
-    },[params.search]);
+  const getSearched = async (name) => {
+    const data = await fetch(
+         `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`);
+         const recipes = await data.json();
+         setSearchedRecipes(recipes.results);
+};
+useEffect(() => {
+  getSearched(params.search);
+}, [params.search]);
 
   return <Grid>
-   {searchRecipes.map((item) =>{
-        return(
-                <Card key= {item.id}>
-                  <Link to= {'/recipe/'+ item.id} >
-                <img src= {item.image} alt= "" />
-                <h4>{item.title}</h4>
-                </Link>
-                </Card>
-        );
-   })}
+    {searchedRecipes.map((item) => {
+      return (
+        <Card key={item.id}>
+          <img src={item.image} alt="" />
+          <h4>{item.title}</h4>
+        </Card>
+      )
+    })}
+  </Grid>
+};
 
-  </Grid>;
-}
+
 const Grid = styled.div`
 display: grid;
 grid-template-columns: repeat(auto-fit, minmax(20rem,1fr));
 grid-grap: 3rem;
 `;
 const Card=styled.div`
-img{
+img {
     width: 100%;
     border-radius: 2rem;
 }
-a{
+a {
     text-decoration: none;
 }
-h4{
+h4 {
 text-align: center;
 padding: 1rem;
 }
-`
+`;
 
 export default Searched;
